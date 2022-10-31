a3c3a8cb70a82f9a51b3b866ebdd94f3703deae8
Denis <d.orel@vents.com.ua>
2022-10-31 09:18:45 +0200

    Add project files.

---
+++ Test/ConvertToPDF.cs
@@ -0,0 +1,63 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+    internal class ConvertToPDF
+    {
+        private static ISldWorks swApp;
+        private ExportPdfData swExportPDFData = default;
+        private ModelDoc2 swModel = default;
+        private ModelDocExtension swModExt = default;
+        private DrawingDoc swDrawDoc = default;
+        private Sheet swSheet = default(Sheet);
+        private bool boolstatus = false;
+        private string[] obj = null;
+        private int errors = 0;
+        private int warnings = 0;
+
+        public bool SaveToPDF(ISldWorks swApp, string pathDoc, string savePath, string pdfFileName)
+        {
+            // Open drawing document
+            swModel = swApp.OpenDoc6(pathDoc, (int)swDocumentTypes_e.swDocDRAWING, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", ref errors, ref warnings);
+            swModExt = swModel.Extension;
+            swExportPDFData = (ExportPdfData)swApp.GetExportFileData((int)swExportDataFileType_e.swExportPdfData);
+
+            if (swModel != null)
+            {
+                // Get the names of the drawing sheets in the drawing
+                // to get the size of the array of drawing sheets
+                swDrawDoc = (DrawingDoc)swModel;
+
+                obj = (string[])swDrawDoc.GetSheetNames();
+                int count = obj.Length;
+                object[] objs = new object[count - 1];
+                DispatchWrapper[] arrObjIn = new DispatchWrapper[count - 1];
+
+                // Activate each drawing sheet, except the last drawing sheet, for
+                // demonstration purposes only and add each sheet to an array
+                // of drawing sheets
+                for (int i = 0; i < count - 1; i++)
+                {
+                    boolstatus = swDrawDoc.ActivateSheet((obj[i]));
+                    swSheet = (Sheet)swDrawDoc.GetCurrentSheet();
+                    objs[i] = swSheet;
+                    arrObjIn[i] = new DispatchWrapper(objs[i]);
+                }
+
+                savePath = savePath + "\\" + pdfFileName + ".pdf";
+
+                // Save the drawings sheets to a PDF file
+                boolstatus = swExportPDFData.SetSheets((int)swExportDataSheetsToExport_e.swExportData_ExportSpecifiedSheets, (arrObjIn));
+                swExportPDFData.ViewPdfAfterSaving = false;
+                swExportPDFData.ExportAs3D = false;
+                boolstatus = swModExt.SaveAs(savePath, (int)swSaveAsVersion_e.swSaveAsCurrentVersion, (int)swSaveAsOptions_e.swSaveAsOptions_Silent, swExportPDFData, ref errors, ref warnings);
+
+                swApp.CloseDoc(swModel.GetTitle());
+            }
+            return boolstatus;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static SldWorks swApp;
+        private static ModelDoc2 swModel;
+        private static DrawingDoc swDraw;
+
+        private static int errors, warnings;
+        private static double Width, Height;
+
+        private static void Main(string[] args)
+        {
+            swApp = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+
+            //string s = @"DXFs\<_FileName_>_<_FeatureName_>_<_ConfName_>_<$CLPRP:Description>.dxf";
+
+            //Regex regex = new Regex(@"<[^>]*>");
+            //MatchCollection matches = regex.Matches(s);
+            //if (matches.Count > 0)
+            //{
+            //    foreach (Match match in matches)
+            //        Console.WriteLine(match.Value);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Совпадений не найдено");
+            //}
+
+            //Master();
+
+            Console.ReadKey();
+        }
+
+        private static void Master()
+        {
+            Height = 0.297; //297×420
+            Width = 0.420;
+
+            string Source2 = @"D:\source\temp\Master\Master_Template_Sheet1.SLDDRW";
+
+            swModel = swApp.OpenDoc6(Source2, (int)swDocumentTypes_e.swDocDRAWING, (int)swOpenDocOptions_e.swOpenDocOptions_Silent, "", ref errors, ref warnings);
+            //swModel = (ModelDoc2)swApp.ActiveDoc;
+
+            swDraw = (DrawingDoc)swModel;
+            swDraw.ActivateSheet("Лист1");
+            swDraw.SetupSheet4("Лист1", 12, 13, 1.0, 1.0, true, "", Width, Height, "По умолчанию");
+            swDraw.EditTemplate();
+
+            //Устанавливаем размеры листа
+            var swDimension = (Dimension)swModel.Parameter("D1@Эскиз1");
+            errors = (int)swDimension.SetSystemValue3(Height - 0.01, (int)swSetValueInConfiguration_e.swSetValue_InAllConfigurations, null);
+
+            swDimension = (Dimension)swModel.Parameter("D2@Эскиз1");
+            errors = (int)swDimension.SetSystemValue3(Width - 0.025, (int)swSetValueInConfiguration_e.swSetValue_InThisConfiguration, null);
+        }
+    }
+}
