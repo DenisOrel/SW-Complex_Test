e81affb3533bfa785ffb4c17e10f4b1e3cf81eca
Denis <d.orel@vents.com.ua>
2022-11-11 17:08:16 +0200

    update

---
+++ SWCore/SWCoreAddin.cs
@@ -0,0 +1,109 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace SampleAddIn
+{
+    [ComVisible(true)]
+    [Guid("AB5D2F7F-B2C4-4CA0-881A-7BFE81A79D73")]
+
+    public class SWAddin : ISwAddin
+    {
+        #region Registration
+
+        private const string ADDIN_KEY_TEMPLATE = @"SOFTWARE\SolidWorks\Addins\{{{0}}}";
+        private const string ADDIN_STARTUP_KEY_TEMPLATE = @"Software\SolidWorks\AddInsStartup\{{{0}}}";
+        private const string ADD_IN_TITLE_REG_KEY_NAME = "SW-Complex";
+        private const string ADD_IN_DESCRIPTION_REG_KEY_NAME = "SW-Complex. Description";
+
+        [ComRegisterFunction]
+        public static void RegisterFunction(Type t)
+        {
+            try
+            {
+                var addInTitle = "";
+                var loadAtStartup = true;
+                var addInDesc = "";
+
+                var dispNameAtt = t.GetCustomAttributes(false).OfType<DisplayNameAttribute>().FirstOrDefault();
+
+                if (dispNameAtt != null)
+                {
+                    addInTitle = dispNameAtt.DisplayName;
+                }
+                else
+                {
+                    addInTitle = t.ToString();
+                }
+
+                var descAtt = t.GetCustomAttributes(false).OfType<DescriptionAttribute>().FirstOrDefault();
+
+                if (descAtt != null)
+                {
+                    addInDesc = descAtt.Description;
+                }
+                else
+                {
+                    addInDesc = t.ToString();
+                }
+
+                var addInkey = Microsoft.Win32.Registry.LocalMachine.CreateSubKey(
+                    string.Format(ADDIN_KEY_TEMPLATE, t.GUID));
+
+                addInkey.SetValue(null, 0);
+
+                addInkey.SetValue(ADD_IN_TITLE_REG_KEY_NAME, addInTitle);
+                addInkey.SetValue(ADD_IN_DESCRIPTION_REG_KEY_NAME, addInDesc);
+
+                var addInStartupkey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(
+                    string.Format(ADDIN_STARTUP_KEY_TEMPLATE, t.GUID));
+
+                addInStartupkey.SetValue(null, Convert.ToInt32(loadAtStartup), Microsoft.Win32.RegistryValueKind.DWord);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error while registering the addin: " + ex.Message);
+            }
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterFunction(Type t)
+        {
+            try
+            {
+                Microsoft.Win32.Registry.LocalMachine.DeleteSubKey(
+                    string.Format(ADDIN_KEY_TEMPLATE, t.GUID));
+
+                Microsoft.Win32.Registry.CurrentUser.DeleteSubKey(
+                    string.Format(ADDIN_STARTUP_KEY_TEMPLATE, t.GUID));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while unregistering the addin: " + e.Message);
+            }
+        }
+
+        #endregion
+
+        private ISldWorks swApp;
+
+        public bool ConnectToSW(object ThisSW, int Cookie)
+        {
+            swApp = ThisSW as ISldWorks;
+
+            swApp.SendMsgToUser("Hello World!");
+
+            return true;
+        }
+
+        public bool DisconnectFromSW()
+        {
+            return true;
+        }
+    }
+}
