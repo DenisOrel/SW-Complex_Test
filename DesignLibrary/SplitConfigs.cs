ce3e5e7271f0dc1d2e3a67895223b8cca50d6b02
Denis <d.orel@vents.com.ua>
2022-11-14 10:36:56 +0200

    save

--- DesignLibrary/SplitConfigs.cs
+++ DesignLibrary/SplitConfigs.cs
@@ -3,10 +3,7 @@ using SolidWorks.Interop.swconst;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DesignLibrary
 {
